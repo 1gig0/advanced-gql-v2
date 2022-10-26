ffeb26aeb51760cd71554858058bb780f357a022
1gig0 <gig.sagrishvili@gmail.com>
2022-10-26 18:19:56 +0400

    feat(unit-tests): add unit tests on the mutation and resolvers

---
+++ tests/resolver.test.js
@@ -0,0 +1,23 @@
+const resolvers = require('../src/resolvers')
+
+describe('resolvers', () => {
+
+  test('feed', () => {
+
+    const result = resolvers.Query.feed(
+      null,
+      null,
+      {
+        models: {
+          Post: {
+            findMany() {
+              return ['hello']
+            }
+          }
+        }
+      }
+    )
+    expect(result).toEqual(['hello'])
+  })
+
+})
